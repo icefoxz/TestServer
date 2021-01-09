f2439d7225905b59b7f3685c45f4e5523ced3f32
Leo Vun <icefoxz@msn.com>
2021-01-09 16:09:12 +0800

    Add project files.

---
+++ TestPing/Function1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace TestPing
+{
+    public static class Function1
+    {
+        [FunctionName("Time")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var now = DateTimeOffset.Now;
+            string responseMessage =
+                "你已经顺利连上服务器了！\n" +
+                $"当前服务器时间是：{now:F}\n" +
+                $"本地时间是:{now.ToLocalTime():F}";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
